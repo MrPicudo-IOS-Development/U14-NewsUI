427a8d613e4b1d291f9728fb2d8250a120674d10
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-02-27 18:52:48 -0600

    Proyecto base
    Creación del proyecto, para el repositorio en GitHub

---
+++ NewsUI/ContentView.swift
@@ -0,0 +1,20 @@
+/* ContentView.swift --> NewsUI. Created by Miguel Torres on 27/02/23. */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsUI/NewsUIApp.swift
@@ -0,0 +1,12 @@
+/* NewsUIApp.swift --> NewsUI. Created by Miguel Torres on 27/02/23. */
+
+import SwiftUI
+
+@main
+struct NewsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
