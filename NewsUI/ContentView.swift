bfaabd985d227b9307580c88b709f17586b978f4
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-02-28 01:30:09 -0600

    Implementación de listas dinámicas
    Se creó una lista dinámica a partir de la documentación de Apple.

--- NewsUI/ContentView.swift
+++ NewsUI/ContentView.swift
@@ -4,11 +4,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(posts){
+                Text($0.title)
+            }
         }
     }
 }
@@ -18,3 +17,37 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Estructura que adopta el protocolo Identifiable, que requiere forsozamente de un atributo llamado id.
+struct Post: Identifiable {
+    let id = UUID() // Le asignamos un identificador único a cada objeto.
+    let title: String
+}
+
+// Arreglo de objetos tipo Post
+let posts = [
+    Post(title: "Hola"),
+    Post(title: "Mundo"),
+    Post(title: "desde"),
+    Post(title: "SwiftUI")
+]
+
+/* Notas de la aplicación:
+ 
+ In every groups can put 10 views if you want to create more than 10 views you should add another group...
+ 
+ Lista simple creada para practicar:
+ List {
+     Text("Objeto 1")
+     Image(systemName: "heart")
+     Text("Objeto 2")
+     Image(systemName: "pencil")
+     Text("Objeto 3")
+     Image(systemName: "folder")
+     Text("Objeto 4")
+     Image(systemName: "paperplane.circle")
+     Text("Objeto 5")
+     Image(systemName: "tray.circle")
+ }
+ 
+ */
